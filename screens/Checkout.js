7d58b76d63742462318e46340bfc6be051f56167
Naphthalina <bnassefuah@st.ug.edu.gh>
2024-07-02 00:49:07 -0700

    Still on checkout page

--- screens/Checkout.js
+++ screens/Checkout.js
@@ -1,10 +1,76 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const Checkout = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>This is the Checkout Page</Text>
+      <Text style={styles.headerText}>CHECKOUT</Text>
+      <View style={styles.bigContainer}>
+        <View style={styles.leftContainer}>
+          <Image 
+            source={require('../assets/images/dress1.png')} // Adjust the path to your image
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text}>Office Wear</Text>
+          <Text style={styles.secondText}>Office wear for your office</Text>
+          <Text style={styles.price}>$120</Text>
+          <Image 
+            source={require('../assets/images/remove.png')} // Adjust the path to your small image
+            style={styles.smallImage}
+          />
+        </View>
+      </View>
+
+      <View style={styles.bigContainer}>
+        <View style={styles.leftContainer}>
+          <Image 
+            source={require('../assets/images/dress4.png')} // Adjust the path to your image
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text}>LAMEREI</Text>
+          <Text style={styles.secondText}>Recycle Boucle Knit Cardigan Pink</Text>
+          <Text style={styles.price}>$120</Text>
+          <Image 
+            source={require('../assets/images/remove.png')} // Adjust the path to your small image
+            style={styles.smallImage}
+          />
+        </View>
+      </View>
+
+      <View style={styles.bigContainer}>
+        <View style={styles.leftContainer}>
+          <Image 
+            source={require('../assets/images/dress3.png')} // Adjust the path to your image
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text}>Church Wear</Text>
+          <Text style={styles.secondText}>Recycle Boucle Knit Cardigan Pink</Text>
+          <Text style={styles.price}>$120</Text>
+          <Image 
+            source={require('../assets/images/remove.png')} // Adjust the path to your small image
+            style={styles.smallImage}
+          />
+        </View>
+      </View>
+
+      <View style={styles.totalContainer}>
+        <Text style={styles.totalTextLeft}>EST . Total</Text>
+        <Text style={styles.totalTextRight}>$240</Text>
+      </View>
+
+      <View style={styles.taskBar}>
+        <Image 
+          source={require('../assets/images/shoppingbagWhite.png')} // Adjust the path to your shopping bag image
+          style={styles.taskBarImage}
+        />
+        <Text style={styles.taskBarText}>CHECKOUT</Text>
+      </View>
     </View>
   );
 }
@@ -12,11 +78,86 @@ const Checkout = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  headerText: {
+    fontSize: 25,
+    marginBottom: 20,
+  },
+  bigContainer: {
+    flexDirection: 'row',
+    width: '90%',
+    height: 200,
+    marginTop: -10,
+    marginLeft: -25,
+  },
+  leftContainer: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  rightContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    position: 'relative', // Add position relative to the right container
+  },
+  image: {
+    width: 140,
+    height: 175,
+  },
   text: {
+    fontSize: 18,
+  },
+  secondText: {
+    fontSize: 14,
+    color: 'grey',
+  },
+  price: {
+    fontSize: 16,
+    color: 'red',
+  },
+  smallImage: {
+    width: 25,
+    height: 25,
+    position: 'absolute', // Position absolute for the small image
+    bottom: 10,
+    right: 10,
+  },
+  totalContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '90%',
+    marginVertical: 20,
+  },
+  totalTextLeft: {
     fontSize: 20,
+    fontWeight: '500',
+    marginBottom:20,
+  },
+  totalTextRight: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  taskBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 50,
+    backgroundColor: 'black',
+  },
+  taskBarImage: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+    borderRadius: 20,
+  },
+  taskBarText: {
+    fontSize: 18,
+    color: 'white',
   },
 });
 
